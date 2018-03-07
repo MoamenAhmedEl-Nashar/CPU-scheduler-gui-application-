cf91ccea6a36db53676754f52a5335a210689b61
Mo'men Ahmed <33075342+MoamenAhmedEl-Nashar@users.noreply.github.com>
2018-03-07 22:54:10 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace scheduler_CPU
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nOfProcesses = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.burstTime_text = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.waitingTime_text = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // nOfProcesses
+            // 
+            this.nOfProcesses.Location = new System.Drawing.Point(202, 66);
+            this.nOfProcesses.Name = "nOfProcesses";
+            this.nOfProcesses.Size = new System.Drawing.Size(100, 22);
+            this.nOfProcesses.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter number of processes:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(324, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(884, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 50);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "click to insert new process";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // burstTime_text
+            // 
+            this.burstTime_text.Location = new System.Drawing.Point(740, 123);
+            this.burstTime_text.Name = "burstTime_text";
+            this.burstTime_text.Size = new System.Drawing.Size(100, 22);
+            this.burstTime_text.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(387, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(347, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "enter burst time of the process and click insert button:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(420, 245);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "average waiting time=";
+            // 
+            // waitingTime_text
+            // 
+            this.waitingTime_text.Location = new System.Drawing.Point(571, 242);
+            this.waitingTime_text.Name = "waitingTime_text";
+            this.waitingTime_text.Size = new System.Drawing.Size(94, 22);
+            this.waitingTime_text.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1051, 410);
+            this.Controls.Add(this.waitingTime_text);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.burstTime_text);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nOfProcesses);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nOfProcesses;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox burstTime_text;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox waitingTime_text;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace scheduler_CPU
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int n;//number of processes
+        private void button1_Click(object sender, EventArgs e)
+        {
+            n = int.Parse(nOfProcesses.Text);
+        }
+        int []burst_time=new int[10];//max number of processe is 10
+        int i = 0;
+        int wtime=0,averageWtime=0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            burst_time[i] = int.Parse(burstTime_text.Text);
+            i++;
+            if (n == i)
+            {
+                button2.Enabled = false;
+                calculateWaitingTime();
+            }
+        }
+        private void calculateWaitingTime()
+        {
+            for(int i=0;i<n;i++)
+            {
+                //gantt chart code
+                averageWtime += wtime;
+                wtime += burst_time[i];
+            }
+            waitingTime_text.Text = (averageWtime / n).ToString();
+        }
+
+    }
+}
